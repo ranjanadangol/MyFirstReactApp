eb50d841bdf56a35a7b9baf48d459beee6e5cf15
Ranjana Dangol <ranjana.nunu@gmail.com>
2016-03-13 09:38:50 +0545

    dynamic styling and dynamic properties

--- src/day-item.js
+++ src/day-item.js
@@ -11,19 +11,36 @@ import React, {
 //create our component
 var DayItem = React.createClass({
 	render: function(){
-		return <Text style={styles.day}>
+		return <Text style={this.style()}>
 			{this.props.day}
 		</Text>	
+	}, 
+	style: function(){
+		return{
+			color: this.color(),
+			fontWeight: this.fontWeight(),
+			fontSize: this.fontSize(),
+			lineHeight: this.lineHeight()
+		}
+	},
+	color: function(){
+		var opacity = 1/this.props.daysUntil;
+		return 'rgba(0,0,0, '+opacity+ ')';
+	},
+	fontWeight: function(){
+		var weight = 7 - this.props.daysUntil;
+		
+		var val= weight * 100;
+		return val.toString();
+	}, 
+	fontSize: function(){
+		return 60 - 6*this.props.daysUntil;
+	}, 
+	lineHeight: function(){
+		return 70 - 4*this.props.daysUntil;
 	}
 });
 
-//style our component
-var styles = StyleSheet.create({
-	day: {
-		fontSize: 18,
-		color: '#0000ff'
-	}
-});
 
 
 //make this code available elsewhere
